dd8f40f5a02271986beea3a6590337c3bf47c77a
takeru0922 <moriai0922takeru@gmail.com>
2020-03-31 16:50:04 +0900

    1/3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :books
-
   root 'post_images#index'#後でログイン成功後のページに書き換える
 
-end
+
+  resources :users, only: [:show, :edit, :update]
+
+
